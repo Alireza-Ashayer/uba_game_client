f4efde0ab6951a973f9e8e3d8adc63cc40bab8f2
Alireza Ashayer <progade@gmail.com>
2016-11-20 20:23:57 +0330

    added app macros

---
+++ Classes/macros/AppMacros.h
@@ -0,0 +1,10 @@
+#ifndef AppMacros_h__
+#define AppMacros_h__
+
+#define NS_UBA_BEGIN namespace nba {
+#define  NS_UBA_END }
+#define  USING_NS_UBA using namespace nba
+
+#define USING_NS_CC_UI using namespace cocos2d::ui
+
+#endif // AppMacros_h__
